b25328f379de6c05fc0e31ce17ccd0efb0195684
_whyvrafvr <jsie@trendev.fr>
2024-11-12 20:06:18 +0100

    propose to assign a new task to a kid

--- main.py
+++ main.py
@@ -3,8 +3,25 @@ This module randomly assigns household tasks to children.
 """
 import random
 
+kids = ["Yann", "Lana", "Alice"]
+tasks = [
+    "Ranger la chambre",
+    "Faire la vaisselle",
+    "Passer l'aspirateur dans la maison",
+    "Balayer le sol de la cuisine",
+    "Sortir les poubelles",
+    "Essuyer la table après les repas",
+    "Laver les vitres",
+    "Aider à préparer le dîner",
+    "Plier et ranger le linge",
+    "Arroser les plantes",
+    "Faire une machine",
+    "Etendre le linge",
+    "Nettoyer la cuisinière"
+]
 
-def assign_tasks(kids, tasks):
+
+def assign_tasks():
     """
     Assigns a random task from the tasks list to each kid in the kids list.
 
@@ -16,33 +33,35 @@ def assign_tasks(kids, tasks):
     dict: A dictionary with kids as keys and their assigned tasks as values.
     """
     # Shuffle the tasks list to ensure random assignment
+    global tasks
     random.shuffle(tasks)
 
     # Assign a task to each kid
-    assigned_tasks = {kid: tasks[i] for i, kid in enumerate(kids)}
+    assigned_tasks = {}
+    for i, k in enumerate(kids):
+        assigned_tasks[k] = tasks[i]
+        tasks = tasks[1:]  # task assigned, can be removed
 
     return assigned_tasks
 
 
-# Assign tasks
-task_assignment = assign_tasks(
-    ["Yann", "Lana", "Alice"],
-    [
-        "Ranger la chambre",
-        "Faire la vaisselle",
-        "Passer l'aspirateur dans la maison",
-        "Balayer le sol de la cuisine",
-        "Sortir les poubelles",
-        "Essuyer la table après les repas",
-        "Laver les vitres",
-        "Aider à préparer le dîner",
-        "Plier et ranger le linge",
-        "Arroser les plantes",
-        "Faire une machine",
-        "Etendre le linge",
-        "Nettoyer la cuisinière"
-    ])
+def print_assigned_tasks(assignement):
+    """
+    Print the assigned tasks
+    """
+    for k, task in assignement.items():
+        print(f"{k}: {task}")
 
-# Print the assigned tasks
-for kid, task in task_assignment.items():
-    print(f"{kid}: {task}")
+
+# Assign tasks
+task_assignment = assign_tasks()
+
+print("\nAttribution initiale des tâches ménagères...")
+print_assigned_tasks(task_assignment)
+
+print("Faut-il changer la tâche d'un enfant? Si oui, saisir le nom de l'enfant")
+kid = input("Choix:")
+
+if kid in task_assignment:
+    task_assignment[kid] = tasks[0]  # assign first task
+    print_assigned_tasks(task_assignment)
